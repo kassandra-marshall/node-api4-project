9718cb36367dddaf0dbe108a8dbd6be6f0448330
dyzn3y <kdmarshall728@gmail.com>
2023-02-27 11:38:00 -0500

    completed project

--- api/server.js
+++ api/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {})
+server.use(cors());
 
-server.post('/api/register', (req, res) => {})
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'I did a get' })
+});
 
-server.post('/api/login', (req, res) => {})
+server.post('/api/register', (req, res) => {
+    res.status(201).json({ message: 'I did a post' })
+});
+
+server.post('/api/login', (req, res) => {
+    res.status(200).json({ message: 'I logged in' })
+});
+
+// server.use('*', (req, res) => {
+//     res.send(`<h1>Hello, there!<h1>`)
+// });
+// NOT WORKING
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        message: error.message,
+        customMessage: 'Something went wrong',
+        stack: error.stack
+    });
+});
 
 module.exports = server
